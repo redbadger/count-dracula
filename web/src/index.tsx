6fa1b3578ba9d8a73c7cc416e83cff12776e52ab
Pedro Martin <pedro.martin@red-badger.com>
2021-03-30 15:27:00 +0100

    Install React router and re order folder structure

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'normalize.css';
+
+import { hydrate, render } from 'react-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Routes from './routes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </React.StrictMode>,
+    rootElement,
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </React.StrictMode>,
+    rootElement,
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ web/src/pages/404.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ErrorPage: React.FC = () => <h1>ErrorPage</h1>;
+
+export default ErrorPage;

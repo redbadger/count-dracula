79eb9c54f2c6f565f205738518c77838dcda813b
Pedro Martin <pedro.martin@red-badger.com>
2021-03-31 15:31:04 +0100

    test render home

--- web/src/pages/home/index.test.tsx
+++ web/src/pages/home/index.test.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
+import React from 'react';
+
 import Home from './index';
 
+import { store } from '../../app/store';
+
 describe('Home', () => {
-  xtest('renders', () => {
-    const { getByText } = render(<Home />);
+  test('renders', () => {
+    const ProviderWrapper: React.FC = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    );
+
+    const { getByText } = render(
+      <ProviderWrapper>
+        <Home />
+      </ProviderWrapper>,
+    );
+
     expect(getByText(/Blood donation/i)).toBeInTheDocument();
   });
 });

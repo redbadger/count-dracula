61825ab249316a3b4a084663d69d50883dc74955
Pedro Martin <pedro.martin@red-badger.com>
2021-03-31 09:32:58 +0100

    Draft a NHS page view

--- web/src/pages/404/index.tsx
+++ web/src/pages/404/index.tsx
@@ -1,11 +1,14 @@
 import { WarningCallout } from 'nhsuk-react-components';
 import React from 'react';
+import Page from '../../components/page';
 
 const ErrorPage: React.FC = () => {
   return (
-    <WarningCallout label="404">
-      <p>Page not Found</p>
-    </WarningCallout>
+    <Page>
+      <WarningCallout label="404">
+        <p>Page not Found</p>
+      </WarningCallout>
+    </Page>
   );
 };
 

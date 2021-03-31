ef41214aec5540e3f1e063b774ef78eb87c377d3
Pedro Martin <pedro.martin@red-badger.com>
2021-03-31 13:50:10 +0100

    Abstract form to a React component

--- web/src/app/features/form/formSlice.ts
+++ web/src/app/features/form/formSlice.ts
@@ -39,9 +39,6 @@ export const formSlice = createSlice({
 
 export const { setValues, setIsReadyToSubmit } = formSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectForm = (state: RootState) => state.form;
 
 export default formSlice.reducer;
--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,46 +1,10 @@
-import {
-  Button,
-  Checkboxes,
-  Col,
-  Container,
-  Row,
-} from 'nhsuk-react-components';
-import React, { SyntheticEvent, useState } from 'react';
+import { Col, Container, Row } from 'nhsuk-react-components';
+import React from 'react';
 
+import Form from '../../components/form';
 import Page from '../../components/page';
 
 const Home: React.FC = () => {
-  const [changeEventLog, setChangeEventLog] = useState<Array<string>>([]);
-  const [inputEventLog, setInputEventLog] = useState<Array<string>>([]);
-  const [currentValue, setCurrentValue] = useState<Array<string>>([]);
-
-  const handleChange = (e: SyntheticEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement;
-    setChangeEventLog([
-      ...changeEventLog,
-      `[${target.id}] Value: ${target.value}, Checked: ${target.checked}`,
-    ]);
-    if (currentValue.includes(target.value)) {
-      setCurrentValue(currentValue.filter((x) => x !== target.value));
-    } else {
-      setCurrentValue([...currentValue, target.value]);
-    }
-  };
-
-  const handleInput = (e: SyntheticEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement;
-    setInputEventLog([
-      ...inputEventLog,
-      `[${target.id}] Value: ${target.value}, Checked: ${target.checked}`,
-    ]);
-  };
-
-  console.log({
-    changeEventLog,
-    inputEventLog,
-    currentValue,
-  });
-
   return (
     <Page>
       <Container>
@@ -51,19 +15,7 @@ const Home: React.FC = () => {
         </Row>
         <Row>
           <Col width="full">
-            <Checkboxes onChange={handleChange} onInput={handleInput}>
-              <Checkboxes.Box value="healthy">
-                Are fit and healthy
-              </Checkboxes.Box>
-              <Checkboxes.Box value="weight">
-                Weight between 7 stone 12 lbs and 25 stone, or 50kg and 158kg
-              </Checkboxes.Box>
-              <Checkboxes.Box value="age">
-                Are aged between 17 and 66 (or 70 if you have given blood
-                before)
-              </Checkboxes.Box>
-            </Checkboxes>
-            <Button>Submit</Button>
+            <Form />
           </Col>
         </Row>
       </Container>

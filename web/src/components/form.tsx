00e960de8b187fea90ee178c4e56d1094d1ce32c
Pedro Martin <pedro.martin@red-badger.com>
2021-03-31 13:50:10 +0100

    Abstract form to a React component

---
+++ web/src/components/form.tsx
@@ -0,0 +1,47 @@
+import { Button, Checkboxes, Form } from 'nhsuk-react-components';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { SyntheticEvent } from 'react';
+
+import {
+  selectForm,
+  setIsReadyToSubmit,
+  setValues,
+} from '../app/features/form/formSlice';
+import { useEffect } from 'react';
+
+const FormComponent: React.FC = () => {
+  const dispatch = useDispatch();
+  const { values, isReadyToSubmit } = useSelector(selectForm);
+
+  const handleChange = (event: SyntheticEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    dispatch(setValues({ ...values, [target.value]: target.checked }));
+  };
+
+  useEffect(() => {
+    const areAllCheckboxesChecked = Object.values(values).every(
+      (value) => value === true,
+    );
+
+    if (areAllCheckboxesChecked) {
+      dispatch(setIsReadyToSubmit());
+    }
+  });
+
+  return (
+    <Form>
+      <Checkboxes onChange={handleChange}>
+        <Checkboxes.Box value="healthy">Are fit and healthy</Checkboxes.Box>
+        <Checkboxes.Box value="weight">
+          Weight between 7 stone 12 lbs and 25 stone, or 50kg and 158kg
+        </Checkboxes.Box>
+        <Checkboxes.Box value="age">
+          Are aged between 17 and 66 (or 70 if you have given blood before)
+        </Checkboxes.Box>
+      </Checkboxes>
+      <Button disabled={!isReadyToSubmit}>Submit</Button>
+    </Form>
+  );
+};
+
+export default FormComponent;

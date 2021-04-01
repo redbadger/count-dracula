d0ca403d14207ab54d4c986b748f9e1aac5fd17c
Pedro Martin <pedro.martin@red-badger.com>
2021-04-01 14:53:47 +0100

    Run sucessfully a11y tests on all urls and printing results on console

--- web/scripts/a11y.sh
+++ web/scripts/a11y.sh
@@ -2,7 +2,7 @@
 yarn workspace web start &
 yarn workspace web run wait-on http://localhost:3000
 server_pid=$!
-yarn workspace web run pa11y-ci --config .pa11yci.json
+yarn workspace web run pa11y-ci --config .pa11yci.json --json
 test_exit_code=$?
 kill $server_pid
 exit $test_exit_code

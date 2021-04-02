0ef04398eade58dc9dd6551cec712e6c3ff357a6
Pedro Martin <pedro.martin@red-badger.com>
2021-04-02 13:09:38 +0100

    Test all business logic

--- web/src/pages/home/index.test.tsx
+++ web/src/pages/home/index.test.tsx
@@ -1,5 +1,10 @@
+import { createMemoryHistory } from 'history';
+// Set TS ignore here due a known bug on TS types for @testing-library/react
+// https: github.com/testing-library/react-testing-library/issues/610#issuecomment-598514192
+// @ts-ignore
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
 import React from 'react';
 
 import { store } from '../../app/store';
@@ -18,5 +23,38 @@ describe('Home', () => {
     );
 
     expect(getByText(/Blood donation/i)).toBeInTheDocument();
+  });
+
+  test('when click an enabled submit button should go to thanks page', async () => {
+    const history = createMemoryHistory();
+    history.push = jest.fn();
+
+    const ProviderWrapper: React.FC = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    );
+
+    render(
+      <ProviderWrapper>
+        <Router history={history}>
+          <Home />
+        </Router>
+      </ProviderWrapper>,
+    );
+
+    const submitButton = screen.getByText('Submit');
+    const checkboxes = [
+      screen.getByLabelText('Are fit and healthy'),
+      screen.getByLabelText(
+        'Weigh between 7 stone 12 lbs and 25 stone, or 50kg and 158kg',
+      ),
+      screen.getByLabelText(
+        'Are aged between 17 and 66 (or 70 if you have given blood before)',
+      ),
+    ];
+
+    checkboxes.forEach((checkbox) => fireEvent.click(checkbox));
+    fireEvent.click(submitButton);
+
+    expect(history.push).toHaveBeenCalledWith('/thanks');
   });
 });

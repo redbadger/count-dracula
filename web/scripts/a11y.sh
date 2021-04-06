52e925451a4fd10d377d4081709add09278783d9
Pedro Martin <pedro.martin@red-badger.com>
2021-04-06 14:59:43 +0100

    Storing a11y results as JSON 💥

--- web/scripts/a11y.sh
+++ web/scripts/a11y.sh
@@ -2,7 +2,7 @@
 yarn workspace web start &
 yarn workspace web run wait-on http://localhost:3000
 server_pid=$!
-yarn workspace web run pa11y-ci --config .pa11yci.json --json
+yarn workspace web run pa11y-ci --json --config .pa11yci.json | tail -1 >> ./test-results/accessability-tests/results.json
 test_exit_code=$?
 kill $server_pid
 exit $test_exit_code

03b2d3682dacfaf64d634f1f8d99f93aef336723
DimplePatel <DimplePatel@users.noreply.github.com>
2021-04-07 16:15:58 +0100

    first tests for checkboxes

--- web/e2e/index.ts
+++ web/e2e/index.ts
@@ -6,6 +6,27 @@ const takeScreenshot = async () => {
   await page.goto('http://localhost:3000');
   await page.screenshot({ path: './e2e/homepage.png' });
 
+// to test validation on checkboxes
+
+  const checkbox1 = (await page.$(".nhsuk-checkboxes__input:first-child"))!;
+  await checkbox1.click();
+  //check checkbox is ticked
+  //check submit button is disabled
+  await page.screenshot({ path: './e2e/checkbox1.png' });
+
+  const checkbox2 = (await page.$(".nhsuk-checkboxes__item:nth-child(2)"))!;
+  await checkbox2.click();
+  //check checkbox is ticked
+  //check submit button is disabled
+  await page.screenshot({ path: './e2e/checkbox2.png' });
+
+  const checkbox3 = (await page.$(".nhsuk-checkboxes__item:nth-child(3)"))!;
+  await checkbox3.click();
+  //check checkbox is ticked
+  //check submit button is disabled
+  await page.screenshot({ path: './e2e/checkbox3.png' });
+
+
   await browser.close();
 };
 

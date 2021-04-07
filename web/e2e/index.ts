1fc70c27b52fd452e100d02b22a8f7a55d9e3d78
DimplePatel <DimplePatel@users.noreply.github.com>
2021-04-07 16:31:38 +0100

    more e2e tests

--- web/e2e/index.ts
+++ web/e2e/index.ts
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
 
-const takeScreenshot = async () => {
+const checkboxValidation = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://localhost:3000');
@@ -10,20 +10,18 @@ const takeScreenshot = async () => {
 
   const checkbox1 = (await page.$(".nhsuk-checkboxes__input:first-child"))!;
   await checkbox1.click();
-  //check checkbox is ticked
-  //check submit button is disabled
+  const disabledButton = await page.$('button[disabled]');
+  const isDisabled = disabledButton !== null;
   await page.screenshot({ path: './e2e/checkbox1.png' });
 
   const checkbox2 = (await page.$(".nhsuk-checkboxes__item:nth-child(2)"))!;
   await checkbox2.click();
-  //check checkbox is ticked
-  //check submit button is disabled
   await page.screenshot({ path: './e2e/checkbox2.png' });
 
   const checkbox3 = (await page.$(".nhsuk-checkboxes__item:nth-child(3)"))!;
   await checkbox3.click();
-  //check checkbox is ticked
-  //check submit button is disabled
+  const enabledButton = await page.$('button:not([disabled])');
+  const isEnabled = enabledButton !== null;
   await page.screenshot({ path: './e2e/checkbox3.png' });
 
 

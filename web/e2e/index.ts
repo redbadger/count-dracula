f1275a4cdbbadc76c25811878ede431e95f99718
DimplePatel <DimplePatel@users.noreply.github.com>
2021-04-08 16:10:18 +0100

    better structure for tests

--- web/e2e/index.ts
+++ web/e2e/index.ts
@@ -1,36 +1,37 @@
 import puppeteer from 'puppeteer';
 
-const checkboxValidation = async () => {
+(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://localhost:3000');
-  await page.screenshot({ path: './e2e/homepage.png'});
+  await page.screenshot({ path: './e2e/e2e-screenshots/homepage.png'});
 
-// tests validation on checkboxes
+  test('ticking the checkboxes one at a time', async () => {
+    const checkbox1 = (await page.$(".nhsuk-checkboxes__input:first-child"))!;
+    await checkbox1.click();
+    const disabledButton = await page.$('button[disabled]');
+    const isDisabled = disabledButton !== null;
+    await page.screenshot({ path: './e2e/e2e-screenshots/checkbox1.png'});
 
-  const checkbox1 = (await page.$(".nhsuk-checkboxes__input:first-child"))!;
-  await checkbox1.click();
-  const disabledButton = await page.$('button[disabled]');
-  const isDisabled = disabledButton !== null;
-  await page.screenshot({ path: './e2e/checkbox1.png'});
+    const checkbox2 = (await page.$(".nhsuk-checkboxes__item:nth-child(2)"))!;
+    await checkbox2.click();
+    await page.screenshot({ path: './e2e/checkbox2.png' });
 
-  const checkbox2 = (await page.$(".nhsuk-checkboxes__item:nth-child(2)"))!;
-  await checkbox2.click();
-  await page.screenshot({ path: './e2e/checkbox2.png' });
+    const checkbox3 = (await page.$(".nhsuk-checkboxes__item:nth-child(3)"))!;
+    await checkbox3.click();
+    const enabledButton = await page.$('button:not([disabled])');
+    const isEnabled = enabledButton !== null;
+    await page.screenshot({ path: './e2e/e2e-screenshots/checkbox3.png'});
+  });
 
-  const checkbox3 = (await page.$(".nhsuk-checkboxes__item:nth-child(3)"))!;
-  await checkbox3.click();
-  const enabledButton = await page.$('button:not([disabled])');
-  const isEnabled = enabledButton !== null;
-  await page.screenshot({ path: './e2e/checkbox3.png'});
+  test('submitting the form', async () => {
+    await page.click('button.nhsuk-button');
+    await page.screenshot({ path: './e2e/e2e-screenshots/thanks.png'});
 
-  // test for submitting and moving to the thanks page, then pressing back button to return to form
-  await page.click('button.nhsuk-button');
-  await page.screenshot({ path: './e2e/thanks.png'});
-
-  const backButton = (await page.$('a.nhsuk-back-link__link'))!;
-  await backButton.click();
-  await page.screenshot({ path: './e2e/returnToForm.png'});
+    const backButton = (await page.$('a.nhsuk-back-link__link'))!;
+    await backButton.click();
+    await page.screenshot({ path: './e2e/e2e-screenshots/returnToForm.png'});
+  });
 
   await browser.close();
-};
+});

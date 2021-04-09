9c861446755f3df244772b42c33a2a781216bf56
DimplePatel <DimplePatel@users.noreply.github.com>
2021-04-09 17:35:59 +0100

    changing e2e bash script

--- web/scripts/e2e.sh
+++ web/scripts/e2e.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-yarn workspace web start &
-yarn workspace web run wait-on http://localhost:3000
 server_pid=$!
-yarn workspace web run jest --config ./e2e/jest.config.js ./e2e/test.ts --json --outputFile=./test-results/e2e-tests/results.json
+yarn workspace web run jest --config ./e2e/jest.config.js ./e2e/test.ts --json
 test_exit_code=$?
 kill $server_pid
 exit $test_exit_code

ee242d51a2aba21b4e2357105532158b685b6446
DimplePatel <DimplePatel@users.noreply.github.com>
2021-04-09 17:41:03 +0100

    trying to get json file into test results folder

--- web/scripts/e2e.sh
+++ web/scripts/e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 server_pid=$!
-yarn workspace web run jest --config ./e2e/jest.config.js ./e2e/test.ts --json
+yarn workspace web run jest --config ./e2e/jest.config.js ./e2e/test.ts --json --outputFile=./test-results/e2eresults.json
 test_exit_code=$?
 kill $server_pid
 exit $test_exit_code

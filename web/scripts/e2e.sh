400a7c11348d6160e73ded7888f5158fd2ad7857
DimplePatel <DimplePatel@users.noreply.github.com>
2021-04-13 10:32:10 +0100

    review changes

--- web/scripts/e2e.sh
+++ web/scripts/e2e.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+yarn workspace web start &
+yarn workspace web run wait-on http://localhost:3000
 server_pid=$!
 yarn workspace web run jest --config ./e2e/jest.config.js ./e2e --json --outputFile=./test-results/e2e-tests/results.json
 test_exit_code=$?
